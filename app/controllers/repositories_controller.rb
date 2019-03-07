e823baed017a93e4b6fd8d8cb89b97da14f0649c
Veniel Dorestave <veniel@noukod.com>
2019-03-07 14:42:34 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,18 +5,17 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    client_id = "b89bbcd8347f4776c767"
-    secret = "f9f3179cee281eba79551c95962cc7b3c1078f7d"
-
-    resp = Faraday.get("https://api.github.com/search/repositories") do |req|
-      req.params["q"] = params[:query]
-      req.params["client_id"] = client_id
-      req.params["client_secret"] = secret
-
-
-    body = JSON.parse(resp.body)
-    @repos = body["items"]
-    render :search
+    def github_search
+      @resp =  Faraday.get 'https://api.github.com/search/repositories' do |req|
+        req.params['q'] = params[:query]
+        req.params['client_id'] = '88539b925d9e9ffbf03f'
+        req.params['client_secret'] = 'ce2bca798709fb5322de17682f9cf780a46bbcdd'
+        req.params['query'] = 'tetris'
+      end
+  
+      body_hash = JSON.parse(@resp.body)
+      @repos = body_hash["items"]
+  
+      render 'search'
   end
-end
 end
